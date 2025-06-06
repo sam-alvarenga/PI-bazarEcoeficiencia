b8fe8eef2b33dd97326d36ef011419bba09280ab
KAILANY DE SOUZA MORAES <kailany.smoraes@senacsp.edu.br>
2025-06-05 22:03:47 -0300

    front da página de login

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/login.css">
+
+    <title>Login Colaboradores</title>
+</head>
+
+<body>
+
+    <div class="form-container">
+        <h2>Login</h2>
+
+
+            <form method="post" class="form-login">
+                <input placeholder="Digite usuário de login" id="login" name="login" required>
+                <input type="password" placeholder="Senha" id="senha" name="senha" required>
+              
+            </form>
+            <button type="submit">Entrar</button>
+
+
+    </div>
+
+</body>
+
+
+</html>
