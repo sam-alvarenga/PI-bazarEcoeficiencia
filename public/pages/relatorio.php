9cbb67f842495fc0c7333deea68b4a02a3d68a52
leticia <leticiaavolio09@gmail.com>
2025-06-09 20:34:12 -0300

    Estilização da página relatorio.php

---
+++ public/pages/relatorio.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relatório - Bazar Ecoeficiência</title>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/reports.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="report-container">
+        <h2>Relatórios</h2>
+        <ul class="report-list">
+            <li class="topic">Top 3 pessoas que mais doaram:
+                <ul>
+                    <li>Benjamin Rollheiser (20 doações)</li>
+                    <li>Gabriel Bontempo (18 doações)</li>
+                    <li>Gabriel Brazão (16 doações)</li>
+                </ul>
+            </li>
+            <li class="topic">Quantidade total de produtos doados:<span>20<span>
+            </li>
+            <li class="topic">Quantidade total de produtos trocados:<span>16<span>
+            </li>
+            <li class="topic">Quantidade de produtos em sobra:
+                <ul>
+                    <li>Acessórios - 15</li>
+                    <li>Mochilas - 3</li>
+                    <li>Vestuário e Calçados - 6</li>
+                    <li>Artigos de Decoração - 9</li>
+                    <li>Eletrônicos e Eletrodomésticos - 5</li>
+                    <li>Brinquedos e Jogos - 7</li>
+                    <li>Livros | DVD | CD | Disco - 10</li>
+                </ul>
+            </li>
+            <li class="topic">Quantidade de trocas por categoria:
+                <ul>
+                    <li>Acessórios - 15</li>
+                    <li>Mochilas - 3</li>
+                    <li>Vestuário e Calçados - 6</li>
+                    <li>Artigos de Decoração - 9</li>
+                    <li>Eletrônicos e Eletrodomésticos - 5</li>
+                    <li>Brinquedos e Jogos - 7</li>
+                    <li>Livros | DVD | CD | Disco - 10</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
