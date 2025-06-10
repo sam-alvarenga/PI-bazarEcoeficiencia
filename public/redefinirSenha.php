86921789fa03aded7dee61d6006e2d75420f2eeb
KAILANY DE SOUZA MORAES <kailany.smoraes@senacsp.edu.br>
2025-06-09 21:32:18 -0300

    estilização das páginas de login e de redefinir senha

--- public/redefinirSenha.php
+++ public/redefinirSenha.php
@@ -8,25 +8,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/login.css">
+    <link rel="stylesheet" href="./assets/css/redefinirSenha.css">
 
     <title>Login Colaboradores</title>
 </head>
 
 <body>
 
-    <div class="form-container">
-        <h2>Login</h2>
+    <div class="form-container " >
+        <h2>Redefinir senha</h2>
 
 
             <form method="post" class="form-login">
                 <input placeholder="Digite o e-mail cadastrado" id="login" name="login" required>
-                <input type="password" placeholder="Senha" id="senha" name="senha" required>
+                <input type="password" placeholder="Nova senha" id="senha" name="senha" required>
               
             </form>
-            <button type="submit">Entrar</button>
+            <button type="submit">Redefinir</button>
 
-            <a href="redefinirSenha.php">Esqueceu sua senha?</a>
+            <a href="index.php">Voltar para a tela de login</a>
+
 
     </div>
 
