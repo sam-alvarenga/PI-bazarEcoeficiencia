ca1232ff0b40af1367c298bb25b41fdd5ab8d35a
Samantha Alvarenga <samantha.c.alvarenga@gmail.com>
2025-08-15 22:19:41 -0300

    feito js da venda e doacao, manipulando os btn das quantidades e trazendo o total

---
+++ public/assets/js/mainScript.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.item-row').forEach(row => {
+    let modalQtd = 0;
+    const input = row.querySelector('.quantity-input');
+    const btnMais = row.querySelector('.round-right');
+    const btnMenos = row.querySelector('.round-left');
+ 
+ 
+    // Botão de aumentar
+    btnMais.addEventListener('click', () => {
+        modalQtd++;
+        input.value = modalQtd;
+        atualizarTotal();
+    });
+ 
+    // Botão de diminuir
+    btnMenos.addEventListener('click', () => {
+        modalQtd--;
+        if (modalQtd < 0) {
+            modalQtd = 0;
+        }
+        input.value = modalQtd;
+        atualizarTotal();
+    });
+ 
+ 
+});
+ 
+// Função para somar todos os valores
+function atualizarTotal() {
+    let total = 0;
+ 
+    document.querySelectorAll('.item-row').forEach(row => {
+        const quantidade = parseInt(row.querySelector('.quantity-input').value) || 0;
+        const preco = parseFloat(row.querySelector('.item-info').dataset.price);
+        total += quantidade * preco;
+    });
+ 
+    document.querySelector('.senacoins-total').textContent = total;
+}
